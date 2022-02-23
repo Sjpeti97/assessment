3a0f564f42b65053f8d16e20c3fbcf4db90dbc3c
Sjpeti97 <sjpeti97@gmail.com>
2022-02-23 13:33:05 +0100

    refactor: delay and format

--- node-todo/models/todos.model.js
+++ node-todo/models/todos.model.js
@@ -1,53 +1,62 @@
 const fs = require('fs/promises');
 const {v4: uuidv4} = require("uuid");
+const {raw} = require("express");
 
 let filePath = '';
+let deleteDelayMs = 300000;
+
+const setDeleteDelayMs = (ms) => {
+    deleteDelayMs = ms;
+}
 
 const setFilePath = (path) => {
     filePath = path;
-}
 
+}
 const checkFilePath = () => {
     if (!filePath) {
         throw 'Filepath is missing'
     }
-}
 
+}
 const saveTodos = (todos) => {
     const raw = JSON.stringify(todos, null, 2);
     return fs.writeFile(filePath, raw);
-}
 
+}
 const loadTodos = async () => {
     checkFilePath();
-    const todos = await fs.readFile(filePath, {encoding: "utf-8"});
-    return JSON.parse(todos);
-}
 
+    try {
+        const todos = await fs.readFile(filePath, {encoding: "utf-8"});
+        return JSON.parse(todos || "[]");
+    } catch {
+        return [];
+    }
+
+}
 const findTodo = async (id) => {
     checkFilePath();
     const allTodos = await loadTodos();
     return allTodos.find((item) => item.id === id);
-}
 
+}
 const deleteTodo = async (id) => {
     const allTodos = await loadTodos();
     const updatedTodos = allTodos.filter((item) => item.id !== id);
     await saveTodos(updatedTodos);
-}
 
+}
 const createTodo = async (text, priority, done) => {
     const todo = assembleTodo(text, priority, done);
     let allTodos = await loadTodos();
     allTodos.push(todo);
     await saveTodos(allTodos);
-}
+    return todo;
 
+}
 const updateTodo = async (id, todo) => {
     let updatedTodo = {};
-    if (!isUpdateValid(todo)) {
-        throw "this value is incorrect or not allowed to be set"
-    }
     const allTodos = await loadTodos();
     const nextItem = allTodos.map((item) => {
         if (item.id !== id) {
@@ -59,9 +68,31 @@ const updateTodo = async (id, todo) => {
     })
     await saveTodos(nextItem);
     deleteDoneAfter5min(updatedTodo);
+    return updatedTodo;
+
+}
+const assembleTodo = (text, priority, done) => {
+    let todo = {}
+    todo.id = uuidv4();
+    todo.text = text;
+    todo.priority = priority;
+    todo.done = done;
+    return todo;
+}
+
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+const deleteDoneAfter5min = async (todo) => {
+    if (todo.done === true) {
+        await delay(deleteDelayMs);
+        if ((await findTodo(todo.id)).done === true) {
+            await deleteTodo(todo.id);
+        }
+    }
 }
 
 module.exports = {
+    setDeleteDelayMs,
     setFilePath,
     loadTodos,
     findTodo,
@@ -69,48 +100,3 @@ module.exports = {
     createTodo,
     updateTodo
 }
-
-const assembleTodo = (text, priority, done) => {
-    let todo = {};
-    const defaultPriority = 3;
-
-    todo.id = uuidv4();
-    todo.text = text;
-
-    if (priority<=5 && priority>=1) {
-        todo.priority = priority;
-    } else {
-        todo.priority = defaultPriority;
-    }
-
-    if (done) {
-        todo.done = done;
-    } else {
-        todo.done = false;
-    }
-    return todo;
-}
-
-const isUpdateValid = (todo) => {
-    let isValid = true;
-    if (todo.id) isValid = false;
-    if (typeof todo.text !== "string") isValid = false;
-    if (todo.priority < 1 || todo.priority > 5) isValid = false;
-    if (typeof todo.done !== "boolean") isValid = false;
-    return isValid;
-}
-
-const deleteDoneAfter5min = (todo) => {
-    console.log("inside delete call");
-    console.log(todo)
-    if (todo.done === true) {
-        console.log("done is true");
-        setTimeout(() => {
-            console.log("inside set timeout");
-            if (findTodo(todo.id).done === true) deleteTodo(todo.id);
-            console.log("deleted")
-        }, 1000);
-        console.log("after timeout call");
-    }
-}
-
