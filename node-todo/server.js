b87a1c35a466835249c942076ed9896f61d4b081
Sjpeti97 <sjpeti97@gmail.com>
2022-02-23 13:32:34 +0100

    refactor: separate app to app and server

---
+++ node-todo/server.js
@@ -0,0 +1,10 @@
+const path = require('path');
+const app = require('./app');
+const {setFilePath} = require("./models/todos.model");
+
+setFilePath(path.join(__dirname, 'todos.json'));
+
+const port = 3000;
+
+app.listen(port);
+
